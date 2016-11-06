0139baedc647b0ea86641fd83db47a6b747dc0af
toni_contreras <tonitrix@hotmail.com>
2016-11-06 16:32:53 +0100

    Add index.html

--- js/main.js
+++ js/main.js
@@ -1,12 +1,14 @@
 angular.module('nfApp', [])
+	.service('Posts', function() {
+		return firebase.database().ref('/users/1' ).once('value').then(function(snapshot) {
+  			return snapshot.val().username;
+		});
+	})
+	
 	.controller('postController', function(posts) {
 		var postList = this;
 
 		postList.posts = posts;
 	})
 
-	.service('Posts', function() {
-		return firebase.database().ref('/users/1' ).once('value').then(function(snapshot) {
-  			return snapshot.val().username;
-		});
-	});
+	
